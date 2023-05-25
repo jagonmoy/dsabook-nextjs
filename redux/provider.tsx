7338572efe7198f1a974bfdb92703a8e15017a51
Jagonmoy <jagonmoydey1997@gmail.com>
2023-05-25 18:33:40 +0600

    delete redux directory

--- redux/provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { setupStore } from "./store";
-import { Provider } from "react-redux";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={setupStore()}>{children}</Provider>;
-}
